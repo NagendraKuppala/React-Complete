3ceb6669fb4e83ddb9a5dc639b3e7e2aee95c33b
NagendraKuppala <nagendrakumar.kuppala@gmail.com>
2023-11-03 14:43:47 -0700

    01_Counter

---
+++ 01_counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+
+  const [count, setCount] = useState(0)
+  
+  const addCount = () => {
+    setCount(count => count + 1);
+  }
+
+  const subCount = () => {   
+      setCount(count => count - 1);
+  }
+
+  return (
+    <>
+        <h1> Counter Project! </h1>
+        <h2> '+' to ADD and '-' to SUBSTRACT </h2>
+        <h3> Counter Value: {count} </h3>
+        <button onClick={addCount}> + </button>
+        <button onClick={subCount}> - </button>
+    </>
+  )
+}
+
+export default App
